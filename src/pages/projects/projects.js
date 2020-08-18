ebae52e56d178aa00e127cb5121a8fd430935fa4
Andrei0872 <andreigtj01@gmail.com>
2020-08-18 20:30:30 +0300

    feat: add `projects` page

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -3,26 +3,31 @@ import Layout from '../../components/layout'
 
 import './projects.css'
 import './project.css'
-import { Link, useStaticQuery } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import Tag from '../../components/tag/'
 
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
-export default function Projects() {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "nir1.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 1500, quality: 100) {
-            ...GatsbyImageSharpFluid
+export default function Projects() {  
+  const { projects: { nodes: projects } } = useStaticQuery(graphql`
+    query FetchProjects {
+      projects: allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/projects/"}}, sort: {order: ASC, fields: frontmatter___order}) {
+        totalCount
+        nodes {
+          fileAbsolutePath
+          frontmatter {
+            title
+            tags
           }
+          html
         }
       }
     }
-  `)
-  
+  `
+  );
+
   return (
     <Layout>
       <div className="c-projects">
@@ -34,22 +39,28 @@ export default function Projects() {
         </div>
         
         <div className="c-projects__content">
-          <article className="c-project">
-            <div className="c-project__header">
-              <h2 className="c-project__title">Project title</h2>
-              <div className="c-project__tags">
-                <Tag className = "c-project__tag c-project__tag--important" > contribution </Tag>
-                <Tag className="c-project__tag">vue</Tag>
-                <Tag className="c-project__tag">nodejs</Tag>
-                <Tag className="c-project__tag">mysql</Tag>
-              </div>
-            </div>
+          {
+            projects.map(proj => (
+              <article className="c-project">
+                <div className="c-project__header">
+                  <h2 className="c-project__title">{proj.frontmatter.title}</h2>
+                  <div className="c-project__tags">
+                    {
+                      proj.frontmatter.tags && 
+                      proj.frontmatter.tags.map(t => (
+                        <Tag className={`c-project__tag ${t === 'contribution' ? 'c-project__tag--important' : ''}`}>{t}</Tag>    
+                      ))
+                    }
+                  </div>
+                </div>
 
-            <div className="c-project__content">
-              <h3 className="c-project__desc-title">Description</h3>
-              <p className="c-project__desc-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, assumenda.</p>
-            </div>
-          </article>
+                <div className="c-project__content">
+                  <h3 className="c-project__desc-title">Description</h3>
+                  <p dangerouslySetInnerHTML={{ __html: proj.html }} className="c-project__desc-content"></p>
+                </div>
+              </article>
+            ))
+          }
         </div>
       </div>
     </Layout>
