08dca0fe766f315c3f274e968c3973a1d4201dc4
Andrei Gatej <andreigtj01@gmail.com>
2020-08-19 10:40:00 +0300

    feat: add `common-pages-layout` component

---
+++ src/components/common-pages-layout/common-pages-layout.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import './common-pages-layout.css'
+
+import { Link } from 'gatsby'
+
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function CommonPagesLayout({ children, title }) {
+  return (
+    <div class="c-common">
+      <div className="c-common__header">
+        <h1 className="c-common__title">{title}</h1>
+        <Link className="c-common__back h-base-link" to="/#work">
+          <FontAwesomeIcon icon={faArrowLeft} /> Work section
+        </Link>
+      </div>
+
+      <div className="c-common__content">{children}</div>
+    </div>
+  )
+}
