5f7720255bd2d27fb0e463f931cf13a1198e247e
Andrei Gatej <andreigtj01@gmail.com>
2021-02-10 15:23:18 +0200

    feat(home-section): remove image

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,21 +2,8 @@ import React from 'react'
 import Img from "gatsby-image"
 
 import './home.css'
-import { useStaticQuery, graphql } from 'gatsby'
 
 export default function Home() {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "me.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
-  
   return (
     <section id="home" className="c-home">
       <div className="c-home__desc">
@@ -24,15 +11,6 @@ export default function Home() {
         <p>
           a <span className="is-important">Software Developer</span> with a passion for solving problems and learning new things.
         </p>
-      </div>
-      <div className="c-home__img">
-        <Img
-          fluid={data.placeholderImage.childImageSharp.fluid}
-          title="Hackathon Dec, 2020"
-        />
-        <div className="c-home__caption">
-          <i>Dec 2018 - my very first hackathon</i>
-        </div>
       </div>
     </section>
   )
