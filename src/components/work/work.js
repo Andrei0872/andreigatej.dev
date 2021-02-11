23cddb446ceb0c11d27023e0067ec5405ffd16d2
Andrei Gatej <andreigtj01@gmail.com>
2021-02-11 13:22:58 +0200

    feat(work): replace cards with subsections

--- src/components/work/work.js
+++ src/components/work/work.js
@@ -1,40 +1,48 @@
 import React from 'react'
 
 import './work.css'
-import Card from '../card/card'
 
-import { faStackOverflow } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBook, faTasks } from '@fortawesome/free-solid-svg-icons';
 import { navigate } from 'gatsby';
 
-const cards = [
+const sections = [
   { 
-    name: 'Stack Overflow', 
-    icon: <FontAwesomeIcon size="2x" icon={faStackOverflow} />, 
+    name: <span>Stack Overflow</span>,
     desc: <p>
       I always find pleasure in <b>helping other developers</b>. 
       It is also a great way to <b>improve skills</b> and <b>discover</b> new approaches to all sorts of problems.
     </p>,
     clickHandler: () => window.open('https://stackoverflow.com/users/9632621/andrei-g%c4%83tej?tab=profile'),
+    buttonText: 'Stack Overflow profile',
   },
   { 
-    name: 'Projects', 
-    icon: <FontAwesomeIcon size="2x" icon={faTasks} />,
+    name: <span>Projects</span>,
     desc: <p>
       Undoubtedly, bringing your idea to life is an ineffable feeling.
       Whether it is about a personal project, a company project or an <b>Open Source</b> project, it's a joy to face the challenges involved.
     </p>,
+    samples: [{ name: 'sample 1', }, { name: 'sample 2', }],
     clickHandler: () => navigate('/projects'),
+    buttonText: 'More projects',
   },
   { 
-    name: 'Blog', 
-    icon: <FontAwesomeIcon size="2x" icon={faBook} />,
+    name: <span>Blog</span>,
     desc: <p>
       My curiosity makes me want to know more about the <b>how</b> and the <b>why</b> behind the tools I'm working with.
       Here I document my journey, share my findings/learnings and much more.
     </p>,
+    samples: [{ name: 'sample 1' }, { name: 'sample 2' }],
     clickHandler: () => navigate('/blog'),
+    buttonText: 'More articles',
+  },
+  {
+    name: <span>My Dev notes</span>,
+    desc: <p>
+      I like to keep track of my learning journey. Considering the abundance of great material that can be found of the Internet, it's difficult to keep up with everything.
+      Here I post things that are worth writing down, from my perspective.
+    </p>,
+    samples: [{ name: 'sample 1' }, { name: 'sample 2' }],
+    clickHandler: () => navigate('/my-dev-notes'),
+    buttonText: 'More notes',
   },
 ];
 
@@ -43,20 +51,25 @@ export default function Work() {
     <section id="work" className="c-work">
       <h1 className="c-work__title">Work</h1>
 
-      <div className="c-work__cards">
-        {
-          cards.map((c, i) => (
-            <Card
-              className="c-work__card"
-              key={i}
-              title={c.name}
-              logo={c.icon}
-              content={c.desc}
-              onClick={c.clickHandler}
-            />
-          ))
-        }
-      </div>
+      {
+        sections.map(
+          s => <div key={s.buttonText} className="c-work-section">
+            <h2 className="c-work-section__name">{s.name}</h2>
+            {s.desc}
+            {
+              s.samples 
+                ? s.samples.map(
+                  // TODO: add click event
+                  sample => <div key={sample.name} className="c-work-section__sample">
+                    <p>{sample.name}</p>
+                  </div>
+                )
+                : null
+            }
+            <button onClick={() => s.clickHandler()} className="c-work-section__button">{s.buttonText}</button>
+          </div>
+        )
+      }
     </section>
   )
 }
