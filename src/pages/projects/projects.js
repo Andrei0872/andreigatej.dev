ea86eab58c5362af85df0c36aa3e50500c4bac9f
Andrei Gatej <andreigtj01@gmail.com>
2021-09-23 16:46:38 +0300

    feat(projects): add a way to add demo photos to projects

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -4,9 +4,9 @@ import Layout from '../../components/layout'
 import './project.css'
 import { graphql, useStaticQuery } from 'gatsby'
 import Tag from '../../components/tag/'
+import ImageSlider from '../../components/image-slider'
 
 import CommonPagesLayout from '../../components/common-pages-layout/common-pages-layout'
-
 
 export default function Projects(props) {  
   const { projects: { nodes: projects } } = useStaticQuery(graphql`
@@ -19,6 +19,7 @@ export default function Projects(props) {
             title
             tags
             slug
+            images_dir
           }
           html
         }
@@ -35,34 +36,52 @@ export default function Projects(props) {
     }, 0);
   }
 
+  const projectsImagesLoader = require.context('/static/projects')
+  const projectsImagesPaths = projectsImagesLoader.keys();
+
   return (
     <Layout>
       <CommonPagesLayout title="Projects">
         {
-          projects.map(proj => (
-            <article id={proj.frontmatter.slug.slice(proj.frontmatter.slug.indexOf('#') + 1)} key={proj.frontmatter.title} className="c-project">
-              <div className="c-project__header">
-                <h2 className="c-project__title">{proj.frontmatter.title}</h2>
-                <div className="c-project__tags">
-                  {
-                    proj.frontmatter.tags && 
-                    proj.frontmatter.tags.map(t => (
-                      <Tag key={t} className={`c-project__tag ${t === 'contribution' ? 'c-project__tag--important' : ''}`}>{t}</Tag>    
-                    ))
-                  }
+          projects.map(proj => {
+            const { images_dir: imagesDir } = proj.frontmatter;
+            const crtProjectImagesPaths = imagesDir
+              ? projectsImagesPaths
+                .filter(p => p.includes(`/${imagesDir}/`))
+                // `.slice(2)` - getting rid of `./`.
+                .map(p => require(`/static/projects/${p.slice(2)}`).default)
+              : [];
+            
+            return (
+              <article id={proj.frontmatter.slug.slice(proj.frontmatter.slug.indexOf('#') + 1)} key={proj.frontmatter.title} className="c-project">
+                <div className="c-project__header">
+                  <h2 className="c-project__title">{proj.frontmatter.title}</h2>
+                  <div className="c-project__tags">
+                    {
+                      proj.frontmatter.tags &&
+                      proj.frontmatter.tags.map(t => (
+                        <Tag key={t} className={`c-project__tag ${t === 'contribution' ? 'c-project__tag--important' : ''}`}>{t}</Tag>
+                      ))
+                    }
+                  </div>
                 </div>
-              </div>
-              <div className="c-project__content">
-                <p 
-                  dangerouslySetInnerHTML={{ 
-                    __html: proj.html
-                      .replace(/<(a|strong)/g, (_, match) => `<${match} class="${match === 'a' ? "is-link" : "is-important" }"`)
-                  }}
-                  className="c-project__desc-content"
-                ></p>
-              </div>
-            </article>
-            ))
+                <div className="c-project__content">
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: proj.html
+                        .replace(/<(a|strong)/g, (_, match) => `<${match} class="${match === 'a' ? "is-link" : "is-important"}"`)
+                    }}
+                    className="c-project__desc-content"
+                  ></p>
+                  <ImageSlider 
+                    /* These are the image properties when taking a screenshot using ALT + PRINT SCREEN */
+                    imageProps={{ width: 1848, height: 1053 }}
+                    imagesPaths={crtProjectImagesPaths}
+                  />
+                </div>
+              </article>
+            )
+          })
           }
       </CommonPagesLayout>
     </Layout>
