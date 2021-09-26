d70b1efe3787bb2ac449867f81fab4d502ebd885
Andrei Gatej <andreigtj01@gmail.com>
2021-09-26 16:42:07 +0300

    fix(blog-template): extract real ids/hrefs from the modified values from `prismjs`

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,16 +1,12 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
-import { defineCustomElements as deckDeckGoHighlightElement } from '@deckdeckgo/highlight-code/dist/loader';
 
 import './blog-template.css';
-
-deckDeckGoHighlightElement();
 
 export default function Template({ data, }) {
   const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
-  
   const parentPath = frontmatter.slug.slice(0, frontmatter.slug.indexOf('/', 1));
   
   return (
@@ -22,7 +18,19 @@ export default function Template({ data, }) {
 
         {parentPath.includes('blog') ? <div className="c-blog-post__date">Published on {new Date(frontmatter.date).toLocaleDateString()}</div> : null}
 
-        <div className="c-blog-post__content" dangerouslySetInnerHTML={{ __html: html }}></div>
+        <div
+          className="c-blog-post__content"
+          dangerouslySetInnerHTML={{
+            // __html: html.replace(/<(a(?![^>]+?href="#)|strong)/g, (_, match) => `<${match} class="${match === 'a' ? "is-link" : "is-important"}"`)
+            __html: html
+              .replace(/<(a(?=[^>]+?(?<!.*class.*)href="(#|https)(?![^>]+?class[^>]+?))|strong)/g, (_, match) => `<${match} class="${match === 'a' ? "is-link" : "is-important"}"`)
+              
+              // The `prismjs` plugin appends & prepends some identification symbols to `href`s and `id`s too.
+              // There RegExps are to extract the actual id from the modifications that `prismjs` does.
+              .replace(/<a[^>]+?href="#([^>]+)?code-classlanguage-text([^>]+?)code([^>"]{0,})"/g, (_, beforeFragment = '', realFragment, afterFragment = '') => `<a href="#${beforeFragment}${realFragment}${afterFragment}"`)
+              .replace(/<(h[1-6])[^>]+?id="([^>]+)?code-classlanguage-text([^>]+?)code([^>"]{0,})"/g, (_, headingTag, beforeId = '', readId, afterId = '') => `<${headingTag} id="${beforeId}${readId}${afterId}"`)
+          }}
+        ></div>
       </div>
     </Layout>
   )
